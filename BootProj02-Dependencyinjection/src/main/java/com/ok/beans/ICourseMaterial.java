45105a2c5e079f4dc5241af6a8712e41f5d81eb1
omkkxr <125791646+omkkkxr@users.noreply.github.com>
2025-02-12 13:38:25 +0530

    pusing

---
+++ BootProj02-Dependencyinjection/src/main/java/com/ok/beans/ICourseMaterial.java
@@ -0,0 +1,9 @@
+package com.ok.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface ICourseMaterial {
+	public String courseMaterial();
+	public double price();
+}
