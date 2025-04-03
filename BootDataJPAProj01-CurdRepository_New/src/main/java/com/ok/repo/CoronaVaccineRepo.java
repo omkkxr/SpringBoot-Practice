017ae255c3027d7314aae9a2a02ad105b559260f
omkkxr <125791646+omkkkxr@users.noreply.github.com>
2025-04-04 01:06:57 +0530

    Sorting repository

--- BootDataJPAProj01-CurdRepository_New/src/main/java/com/ok/repo/CoronaVaccineRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ok.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ok.entity.CoronaVaccine;
-
-public interface CoronaVaccineRepo extends CrudRepository<CoronaVaccine,Long>{
-
-}
--- BootDataJPAProj1-CurdRepository/src/main/java/com/ok/service/ICoronaVaccineMgmtService.java
+++
@@ -1,8 +0,0 @@
-package com.ok.service;
-
-import com.ok.entity.CoronaVaccine;
-
-
-public interface ICoronaVaccineMgmtService {
-	public String registerVaccine(CoronaVaccine vaccine );
-}
